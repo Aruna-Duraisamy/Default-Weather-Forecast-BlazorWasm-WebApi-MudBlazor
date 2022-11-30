da8e0daae5e3f723f3b3de71df3b00e2909dcb34
arunaganeshraj <aruna.durai@gmail.com>
2022-11-30 09:56:52 +0000

    Added appsettings.json

--- WeatherApp.Web/Program.cs
+++ WeatherApp.Web/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5103") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder!.Configuration["baseAddress"]!) });
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
